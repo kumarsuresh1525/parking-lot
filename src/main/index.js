1c2e3ddd6252dd0e87f7cec79d22a4303b7dd143
suresh <suresh_kumar@external.mckinsey.com>
2020-01-15 15:14:58 +0530

    PL-000 [Suresh] : Added create parking lot method

--- src/main/index.js
+++ src/main/index.js
@@ -9,6 +9,19 @@ class Solution {
     this.availableSlots = [];
     this.carDetails = [];
   }
+
+  createParkingLot(noOfLot) {
+    try {
+      if (isNaN(noOfLot)) throw new Error('Value should be number')
+      this.maxSize = parseInt(noOfLot);
+      for (let i = 0; i < this.maxSize; i++) {
+        this.availableSlots.push(i);
+      }
+      return `Created a parking lot with ${this.availableSlots.length} slots`;
+    } catch (error) {
+      return 'Value should be Number';
+    }
+  }
 }
 
 module.exports = Solution;
